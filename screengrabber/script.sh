f0e60dd8a6e4b6ab19061d3b43189a342df11c72
Patrick Veverka <pveverka@hashicorp.com>
2020-01-22 17:32:51 -0500

    updates

--- screengrabber/script.sh
+++ screengrabber/script.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 data_dir="$(dirname $(dirname $(realpath $0)) )/images"
-echo "DATA: $data_dir"
 
+echo "SCRAPING TO DIRECTORY: $data_dir"
+
+echo "CLEARING $data_dir"
 rm $data_dir/*.jpg
 
-ffmpeg -i "https://e1-na8.angelcam.com/m7-na3/cameras/54700/streams/hls/playlist.m3u8?token=eyJjYW1lcmFfaWQiOiI1NDcwMCIsInRpbWUiOjE1Nzk3MjMxMTE3MDQ5MDYsInRpbWVvdXQiOjM2MDB9%2Ef01c622f5307fe0071bfae94a41c80968755ca3128b0ec6b73899f8796f5cd3b" -vf fps=1/10 $data_dir/bigtexan_%04d.jpg -hide_banner
+echo "GETTING UPDATED URL"
+url=$(curl -s https://v.angelcam.com/iframe\?v\=9klzdgn2y4\&autoplay\=1 | grep 'source: ' | grep -Eo 'https://[^/"]+.*' | cut -d \' -f 1)
+
+echo "FOUND $url"
+echo "GRABBING SCREENSHOTS"
+
+ffmpeg -i $url -vf fps=1/20 $data_dir/bigtexan_$(date +%s)_%05d.jpg -hide_banner
