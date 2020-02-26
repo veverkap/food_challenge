a6eeb274304ac9023c702c011ef40b1feb1ca92f
Patrick Veverka <pveverka@hashicorp.com>
2020-02-25 23:49:54 -0500

    files

--- meatsweatsweb/build.sh
+++ meatsweatsweb/build.sh
@@ -1,5 +1,5 @@
 docker build -t meatsweatsweb .
-docker tag cvweb registry.veverka.net/cvweb
-docker push registry.veverka.net/cvweb
-# docker run -d --name mycontainer -p 8080:80 cvweb
+docker tag meatsweatsweb registry.veverka.net/meatsweatsweb
+docker push registry.veverka.net/meatsweatsweb
+# docker run -d --name mycontainer -p 8080:80 meatsweatsweb
 # docker logs -f mycontainer
