fc1f90bc19ea1b3b1f9576df4d8db3ad88003e2d
Patrick Veverka <patrick@veverka.net>
2020-03-02 16:43:58 -0500

    files

--- meatsweatsweb/app/main.py
+++ meatsweatsweb/app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI, File, UploadFile
 from starlette.requests import Request
 from app.detecting_resource import DetectingResource
 import os
+import cv2
 import logging
 
 app = FastAPI()
 logger = logging.getLogger("fastapi")
 logger.setLevel(logging.DEBUG)
+print("OpenCV Version: {}".format(cv2.__version__))
 
 
 @app.get("/")
--- meatsweatsweb/build.sh
+++ meatsweatsweb/build.sh
@@ -1,5 +1,5 @@
-docker build -t meatsweatsweb .
-docker tag meatsweatsweb registry.veverka.net/meatsweatsweb
-docker push registry.veverka.net/meatsweatsweb
-# docker run -d --name mycontainer -p 8080:80 meatsweatsweb
+docker build -t meatsweatweb .
+docker tag meatsweatweb registry.veverka.net/meatsweatweb
+docker push registry.veverka.net/meatsweatweb
+# docker run -d --name mycontainer -p 8080:80 meatsweatweb
 # docker logs -f mycontainer
